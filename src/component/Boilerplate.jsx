a3e8c8de6fc2f335517a46a752e32d8a0e9ed4f3
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-04-27 11:57:29 +0530

    Updated Boiler Plate loading

--- src/component/Boilerplate.jsx
+++ src/component/Boilerplate.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios';
 import {useParams} from 'react-router-dom'
 
 function Boilerplate() {
@@ -18,7 +19,15 @@ function Boilerplate() {
   return (
     <>
      <div className='bg-[#000000] h-screen'>
+     <div className='h-full w-full flex flex-col items-center place-content-center'>
+       <div className='h-52 w-52 bg-white rounded-xl animate-pulse'>
 
+       </div>
+       <div className='text-white mt-48 animate-bounce text-5xl'>
+           CHECKING IF THE LINK IS VALID OR NOT
+       </div>
+        
+    </div>
      </div>
     </>
   )
