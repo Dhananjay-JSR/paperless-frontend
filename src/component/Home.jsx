3b026ba520202eb42884a080943db1193aeeba60
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-04-28 13:29:25 +0530

    POST REQUEST COMPLETED

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useRef,useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { globalStyles } from "./style/globalReset";
 import { Button } from "./style/Button";
 import { Header } from "./style/Header";
@@ -10,162 +10,243 @@ import { Content1 } from "./style/Content1";
 import { Content2 } from "./style/Content2";
 import { Content_1_HeadLine as HeadLine } from "./style/Content_1_HeadLine";
 import { InputTextBox } from "./style/InputTextBox";
-import {  keyframes, styled } from "@stitches/react";
+import { keyframes, styled } from "@stitches/react";
 import { Loader } from "./style/loader";
 import axios from "axios";
 function Home() {
+  const [DarkModeValue, SetDarkModeValue] = useContext(DarkMode);
+  const [open, setopen] = useState(false);
+  const [msgBox, setmsgBox] = useState("");
 
-  const [DarkModeValue,SetDarkModeValue]= useContext(DarkMode)
-  const [open, setopen] = useState(false)
-  const [msgBox, setmsgBox] = useState('')
-  
-  const passInput = useRef()
-  const [received_data, setreceived_data] = useState(false)
-  const [request_sent, setrequest_sent] = useState(false)
-  const [disabled, setdisabled] = useState(false)
+  const passInput = useRef();
+  const [received_data, setreceived_data] = useState(false);
+  const [request_sent, setrequest_sent] = useState(false);
+  const [disabled, setdisabled] = useState(false);
+  const [notifyTimeout, setnotifyTimeout] = useState(false)
   // let request_sent = false;
   globalStyles();
   function ThemeChanger() {
-    SetDarkModeValue(!DarkModeValue)
+    SetDarkModeValue(!DarkModeValue);
   }
 
-  const [hashedLink, sethashedLink] = useState("")
-function submitData(){
-  setrequest_sent(!request_sent)
-  console.log(passInput.current.value)
-  console.log(msgBox)
-  navigator.clipboard.writeText("Hello")
-  let content = {
-    "message": msgBox,
-    "password": +passInput.current.value
+  const [hashedLink, sethashedLink] = useState("");
+  function submitData() {
+    setrequest_sent(!request_sent);
+    // console.log(passInput.current.value);
+    // console.log(msgBox);
+    
+    let content = {
+      message: msgBox,
+      password: +passInput.current.value,
+    };
+    axios
+      .post("https://paperless-backend-mongo.up.railway.app/storage", content)
+      .then(function (response) {
+        setreceived_data(response.data.HashedLink);
+        navigator.clipboard.writeText(response.data.HashedLink);
+        // console.log(response.data.HashedLink);
+        setTimeout(() => {
+          setnotifyTimeout(true)
+        }, 3000);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  //NOTICATION
+const revealNotification= keyframes({
+  "0%":{
+    transform: 'translate(0,-50px)',
+    opacity: 0
+  },
+  "100%":{transform: 'translate(0,0)',
+opacity: "100%"
 }
-  axios.post('https://paperless-backend-mongo.up.railway.app/storage', content)
-  .then(function (response) {
-    setreceived_data(response.data.HashedLink)
-    console.log(response.data.HashedLink)
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-//MODAL
-const revealModal = keyframes({
-  "0%" : {opacity: '0%'},
-  "100":{opacity:'100%'}
 })
-  const Modal = styled('div',{
-    position: 'fixed',
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+
+
+const Notification = styled('div',{
+  position: 'fixed',
+  right: '100px',
+  zIndex: 10,
+  width: '500px',
+  height: '50px',
+  borderRadius: '15px',
+  fontSize: '30px',
+  fontFamily: 'system-ui',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  top: '150px',
+ 
+  variants:{
+    "darkmode":{
+      true:{
+
+        border: '2px solid black',
+        background: '#f5f5f5',
+        color: 'Black'
+      },
+      false:{
+        border: '2px solid white',
+        background: '#151718',
+        color: 'white'
+      }
+    },
+    "ending":{
+      true:{
+        animation: `${revealNotification} 2s reverse`,
+        animationFillMode: 'forwards'
+      },
+      false:{
+        animation: `${revealNotification} 2s `,
+      }
+    }
+  }
+
+})
+
+
+
+  //NOTIFICATION END HERE
+  //MODAL
+  const revealModal = keyframes({
+    "0%": { opacity: "0%" },
+    100: { opacity: "100%" },
+  });
+  const Modal = styled("div", {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
     zIndex: 7,
-     backdropFilter: 'blur(15px)',
+    backdropFilter: "blur(15px)",
     animation: `${revealModal} 0.2s 1 `,
     variants: {
-      "display":{
-        false:{
-          display: 'none'
+      display: {
+        false: {
+          display: "none",
         },
-        true:{
-          display: 'flex'
-        }
+        true: {
+          display: "flex",
+        },
       },
-    }
-  })
+    },
+  });
 
-const ModalWindow=styled('div',{
-  backgroundColor: 'Yellow',
-  height: '20rem',
-  width: '35rem',
-  borderRadius: '25px',
-  position: 'relative',
-  display: 'grid',
-  justifyContent: 'center',
-  alignItems: 'center',
-    variants:{
-    "darkMode":{
-      false: {
-        backgroundColor: '#151718',
-        border: '2px solid white',
-        color: 'White'
+  const ModalWindow = styled("div", {
+    backgroundColor: "Yellow",
+    height: "20rem",
+    width: "35rem",
+    borderRadius: "25px",
+    position: "relative",
+    display: "grid",
+    justifyContent: "center",
+    alignItems: "center",
+    variants: {
+      darkMode: {
+        false: {
+          backgroundColor: "#151718",
+          border: "2px solid white",
+          color: "White",
+        },
+        true: {
+          backgroundColor: "#f5f5f5",
+          color: "Black",
+          border: "2px solid black",
+        },
       },
-      true:{
-        backgroundColor: '#f5f5f5',
-        color: 'Black',
-        border: '2px solid black'
-      }
-  }
-  }
-
-})
+    },
+  });
 
   //MODAL
   return (
     <>
-    <Modal display={open}>
-        <ModalWindow darkMode={DarkModeValue} >
-        <TextContainer css={{
-          textAlign: 'center',
-          marginTop: '10px',
-          fontSize: 'xx-large',
-          fontWeight: 'bold',
-          fontFamily: 'sans-serif',
-        }}>
-          ENTER YOUR PASSWORD
-
-        </TextContainer>
-        {request_sent? 
-        <TextContainer css={{
-          width:'500px',
-          height: '60px',
-          overflowY: 'hidden',
-          marginBottom: '100px',
-          fontSize: '25px',
-          textAlign: 'center',
-          fontWeight: 'bold'
-        }}>
+    {received_data?  <Notification ending={notifyTimeout} darkmode={DarkModeValue}>
+      Link Added to Clipboard
+    </Notification> : ""}
+      <Modal display={open}>
+        <ModalWindow darkMode={DarkModeValue}>
+          <TextContainer
+            css={{
+              textAlign: "center",
+              marginTop: "10px",
+              fontSize: "xx-large",
+              fontWeight: "bold",
+              fontFamily: "sans-serif",
+            }}
+          >
+            {received_data?"Copy Your Link" : "ENTER YOUR PASSWORD"}
+            
+          </TextContainer>
+          {request_sent ? (
+            <TextContainer
+              css={{
+                width: "500px",
+                height: "60px",
+                overflowY: "hidden",
+                marginBottom: "100px",
+                fontSize: "21px",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              {received_data
+                ? received_data
+                : "Waiting for Server to Respond Back"}
+            </TextContainer>
+          ) : (
+            <InputTextBox
+              css={{
+                width: "400px",
+                height: "20px",
+                overflowY: "hidden",
+                marginBottom: "100px",
+              }}
+              placeholder="ENTER PASSWORD TO ENCRYPT IT"
+              ref={passInput}
+              disabled={disabled}
+              darkMode={DarkModeValue}
+            ></InputTextBox>
+          )}
+          {received_data? "" :
+          <Button
           
-        {received_data? received_data :"Waiting for Server to Respond Back"}
-        </TextContainer>
-       
-        :  <InputTextBox css={
-          {
-            width:'400px',
-            height: '20px',
-            overflowY: 'hidden',
-            marginBottom: '100px'
-         }
-        } 
-        
-        placeholder="ENTER PASSWORD TO ENCRYPT IT" ref={passInput} disabled={disabled} darkMode={DarkModeValue}>
-            </InputTextBox>
-      }
-        <Button
-        darkMode={DarkModeValue}
-        onClick={()=>{if (passInput.current.value===''){window.alert("Enter Valid password")}else {submitData()}}}
-         css={{
-          position: 'absolute',
-          bottom: '20px',
-          right: '180px'
-        }}>
-          {request_sent? <Loader/>:`` }
-          <div>
-          {request_sent? ``:`Submit` }
-          </div>
-        </Button>
-        <Button
-        darkMode={DarkModeValue}
-        onClick={()=>{setopen(prev=>!prev)}}
-        css={{
-          position: 'absolute',
-          bottom: '20px',
-          right: '40px'
-        }}>
-          Cancel
-        </Button>
+            darkMode={DarkModeValue}
+            onClick={() => {
+              if (passInput.current.value === "") {
+                window.alert("Enter Valid password");
+              } else {
+                submitData();
+              }
+            }}
+            css={{
+              position: "absolute",
+              bottom: "20px",
+              right: "180px",
+            }}
+          >
+            {request_sent ? <Loader darkMode={DarkModeValue} /> : ``}
+            <div>{request_sent ? `` : `Submit`}</div>
+          </Button>
+          }
+          <Button
+            darkMode={DarkModeValue}
+            onClick={() => {
+              setopen((prev) => !prev);
+              setrequest_sent(false);
+              setreceived_data(false)
+            }}
+            css={{
+              position: "absolute",
+              bottom: "20px",
+              right: "40px",
+            }}
+          >
+            Cancel
+          </Button>
         </ModalWindow>
-    </Modal>
+      </Modal>
       <Header darkMode={DarkModeValue}>
         <TextContainer>
           PaperLess
@@ -173,7 +254,7 @@ const ModalWindow=styled('div',{
             css={{
               position: "fixed",
               right: "60px",
-              top: '1.25rem'
+              top: "1.25rem",
             }}
             onClick={ThemeChanger}
             darkMode={DarkModeValue}
@@ -183,67 +264,81 @@ const ModalWindow=styled('div',{
         </TextContainer>
       </Header>
       <Container>
-            <Content1 darkMode={DarkModeValue}>
-            <HeadLine darkMode={DarkModeValue}>
-              INTRODUCING
-            </HeadLine>
-            <HeadLine 
+        <Content1 darkMode={DarkModeValue}>
+          <HeadLine darkMode={DarkModeValue}>INTRODUCING</HeadLine>
+          <HeadLine
             css={{
-              fontSize: '100px',
-              background: 'linear-gradient(90deg, #FEAC5E 0%, #C779D0 50%, #4BC0C8 100%)',
-              '-webkit-background-clip': 'text',
-              '-webkit-text-fill-color': 'transparent',
-              animationDelay: '2s',
-              animationTimingFunction: 'ease-in-out'
-            }}>
+              fontSize: "100px",
+              background:
+                "linear-gradient(90deg, #FEAC5E 0%, #C779D0 50%, #4BC0C8 100%)",
+              "-webkit-background-clip": "text",
+              "-webkit-text-fill-color": "transparent",
+              animationDelay: "2s",
+              animationTimingFunction: "ease-in-out",
+            }}
+          >
             PaperLess
-            </HeadLine >
-            <HeadLine darkMode={DarkModeValue}>
-            A MODERN PASTEBIN
-            </HeadLine>
-            <HeadLine darkMode={DarkModeValue}>
-            PRIMARILY FOCUS ON 
-            </HeadLine>
-            <HeadLine darkMode={DarkModeValue}>
-            SECURITY
-            </HeadLine>
-            </Content1>
-            <Content2 darkMode={DarkModeValue}>
-            <HeadLine
+          </HeadLine>
+          <HeadLine darkMode={DarkModeValue}>A MODERN PASTEBIN</HeadLine>
+          <HeadLine darkMode={DarkModeValue}>PRIMARILY FOCUS ON</HeadLine>
+          <HeadLine darkMode={DarkModeValue}>SECURITY</HeadLine>
+        </Content1>
+        <Content2 darkMode={DarkModeValue}>
+          <HeadLine
             darkMode={DarkModeValue}
             css={{
               fontSize: "60px",
-              fontWeight: 'bold',
+              fontWeight: "bold",
               marginTop: "70px",
-              width: 'max-content',
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              marginBottom: '70px',
-              animation: 'none'
-            }}>
-              Ready to Experience the Future ?
-            </HeadLine>
-            <InputTextBox placeholder="INSERT YOUR NOTE HERE" darkMode={DarkModeValue} value={msgBox} onChange={(e)=>{setmsgBox(e.target.value)}}>
-            </InputTextBox>
-            <Button
-            onClick={()=>{ if (msgBox===''){window.alert("Enter Valid Message")} else {setopen(prev=>!prev)}}}
+              width: "max-content",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginBottom: "70px",
+              animation: "none",
+            }}
+          >
+            Ready to Experience the Future ?
+          </HeadLine>
+          <InputTextBox
+            placeholder="INSERT YOUR NOTE HERE"
+            darkMode={DarkModeValue}
+            value={msgBox}
+            onChange={(e) => {
+              setmsgBox(e.target.value);
+            }}
+          ></InputTextBox>
+          <Button
+            onClick={() => {
+              if (msgBox === "") {
+                window.alert("Enter Valid Message");
+              } else {
+                setopen((prev) => !prev);
+              }
+            }}
             css={{
-            width: 'fit-content',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            marginTop: '30px'
+              width: "fit-content",
+              marginLeft: "auto",
+              marginRight: "auto",
+              marginTop: "30px",
             }}
             darkMode={DarkModeValue}
-            >
-              Let's Go !
-            </Button>
-            </Content2>
+          >
+            Let's Go !
+          </Button>
+        </Content2>
       </Container>
       <Footer darkMode={DarkModeValue}>
-        Made By <TextContainer as="a" href="https://github.com/Dhananjay-JSR/paperless-frontend"> Dipesh </TextContainer> With ❤
+        Made By{" "}
+        <TextContainer
+          as="a"
+          href="https://github.com/Dhananjay-JSR/paperless-frontend"
+        >
+          {" "}
+          Dipesh{" "}
+        </TextContainer>{" "}
+        With ❤
       </Footer>
       <div></div>
-  
     </>
   );
 }
--- src/component/style/loader.jsx
+++ src/component/style/loader.jsx
@@ -4,10 +4,20 @@ const spinner = keyframes({
     '100%':{transform: 'rotate(360deg)'}
 })
 export const Loader = styled('div',{
-    border: '8px solid #f3f3f3',
-    borderTop: '8px solid #3498db',
     borderRadius: '50%',
     width: '5px',
     height: '5px',
     animation: `${spinner} 3s infinite`,
+    variants:{
+        "darkMode":{
+            true:{
+                border: '8px solid #151718',
+                borderTop: '8px solid White',
+            },
+            false:{
+                border: '8px solid #d2d2d2',
+                borderTop: '8px solid Black',
+            }
+        }
+    }
 })
