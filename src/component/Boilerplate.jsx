10352cecafd944582aa997f3854857439689e429
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-04-29 18:24:25 +0530

    test

--- src/component/Boilerplate.jsx
+++ src/component/Boilerplate.jsx
@@ -24,23 +24,47 @@ function Boilerplate() {
   const { id } = useParams();
 
   function OnValidateSubmit() {
-      axios
-      .post(`${API_URL}/Storage/${id}`,{
-        password: PassRef.current.value
-      },{
-        withCredentials: true
-      } )
-      .then((res)=>{
+      // axios
+      // .post(`${API_URL}/Storage/${id}`,{
+      //   password: PassRef.current.value
+      // },{
+      //   withCredentials: true
+      // } )
+      // .then((res)=>{
        
-        setreceivedObj(res.data)
-      }).catch((err)=>{
-        if (err.response.status===469){
-          window.alert("Token Expired Please Refresh Page")
-        }else if (err.response.status===410){
-          window.alert("Password Didn't match")
-        }
-      })
+      //   setreceivedObj(res.data)
+      // }).catch((err)=>{
+      //   if (err.response.status===469){
+      //     window.alert("Token Expired Please Refresh Page")
+      //   }else if (err.response.status===410){
+      //     window.alert("Password Didn't match")
+      //   }
+      // })
 
+      async function postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: 'include', // include, *same-origin, omit
+          headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          redirect: 'follow', // manual, *follow, error
+          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+          body: JSON.stringify(data) // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+      }
+      
+      postData(`${API_URL}/Storage/${id}`,{
+          password: PassRef.current.value
+        })
+        .then(data => {
+          console.log(data); // JSON data parsed by `data.json()` call
+        });
 
 
 
