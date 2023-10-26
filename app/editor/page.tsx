b18760718a8b66f7021b513202026c5b343dfe81
Dhananjay-JSR <ajaysenday@gmail.com>
2023-10-27 00:11:42 +0530

    Added Index Page

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,6 +1,39 @@
+'use client'
+import Editor, { DiffEditor } from '@monaco-editor/react';
+import { useRef } from 'react';
+
 export default function Home() {
-    return <><div className="w-">
-        Hello World
-        </div>
-        </>
+   
+    const diffEditorRef = useRef(null);
+
+  function handleEditorDidMount(editor, monaco) {
+    diffEditorRef.current = editor;
+  }
+
+  function showOriginalValue() {
+    alert(diffEditorRef.current.getOriginalEditor().getValue());
+  }
+
+  function showModifiedValue() {
+    alert(diffEditorRef.current.getModifiedEditor().getValue());
+  }
+
+  return (
+    <>
+      <div className='bg-[#212121]'>
+      {/* <Editor
+        className='h-screen'
+      defaultLanguage="javaScript"
+      defaultValue="// let's write some broken code 😈"
+      onValidate={(markers)=> {
+        // model markers
+        markers.forEach((marker) => console.log('onValidate:', marker.message));
+      }}
+    /> */}
+
+
+
+      </div>
+    </>)
+
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+import { Button, Divider } from '@nextui-org/react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
    <>
-   Hello</>
+   <div className='max-w-7xl mx-auto p-4'>
+    <div className='text-center mt-12 font-medium text-3xl'>
+
+   Share Code with <span className='from-[#9ae7ff] to-[#f06dee] bg-clip-text text-transparent bg-gradient-to-b tracking-tight inline font-semibold'> Elegance</span>
+   <br/>
+Seamless  <span className='from-[#7de5e0] to-[#0bffd6] bg-clip-text text-transparent bg-gradient-to-b tracking-tight inline font-semibold'>Code Sharing</span> Made Easy
+<br/>
+Explore <span className='from-[#b1e1f3] to-[#2ba8fb] bg-clip-text text-transparent bg-gradient-to-b tracking-tight inline font-semibold'>'Kode' </span>- Your Code's New Home"
+    </div>
+<div className='flex w-full max-w-md justify-between mx-auto mt-16'>
+
+    <Button color='primary' className=''>Get Started</Button>
+    <Button color='secondary' className=''>Learn More</Button>
+</div>
+    <Divider className='mt-36'/>
+   </div>
+   {/* Hello */}
+   </>
   )
 }
