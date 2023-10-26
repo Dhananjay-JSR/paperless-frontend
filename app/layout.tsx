e786559f44895b4e2bbf3361b3652099efc870ca
Dhananjay-JSR <ajaysenday@gmail.com>
2023-10-26 22:17:43 +0530

    Added Theme Context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Image from 'next/image'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Providers>
       {children}
       <div className='relative flex flex-col'>
           <div className='fixed  block opacity-70 '>
@@ -28,7 +30,7 @@ export default function RootLayout({
           </div>
         </div>
 
-
+        </Providers>
       </body>
     </html>
   )
