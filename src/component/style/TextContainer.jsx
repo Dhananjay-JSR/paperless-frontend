55cc7fbc41f9ff7101832797bf8df2f0962917a1
Dhananjay-JSR <ajaysenday@gmail.com>
2023-10-26 18:27:32 +0530

    Migrated to Server Component

--- src/component/style/TextContainer.jsx
+++
@@ -1,7 +0,0 @@
-import { styled } from "@stitches/react"
-export const TextContainer = styled('div',{
-    height: 'min-content',
-    paddingLeft: '1vh',
-    paddingRight: '1vh',
-    textDecoration: 'none',
-  })
--- src/util/ip.js
+++
@@ -1,1 +0,0 @@
-export const API_URL = "https://paperless-backend-production.up.railway.app";
