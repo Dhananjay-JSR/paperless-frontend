832b373cd0504ac48fcba4e0efe284cabf875faf
Dhananjay-JSR <ajaysenday@gmail.com>
2023-10-27 02:04:20 +0530

    API Route Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Button, Divider } from '@nextui-org/react'
 import Image from 'next/image'
+import { CheckCircledIcon } from '@radix-ui/react-icons'
 
 export default function Home() {
   return (
    <>
    <div className='max-w-7xl mx-auto p-4 flex-grow '>
-    <div className='text-center mt-12 font-medium text-3xl'>
+    <div className='text-center mt-24 font-medium text-3xl'>
 
    Share Code with <span className='from-[#9ae7ff] to-[#f06dee] bg-clip-text text-transparent bg-gradient-to-b tracking-tight inline font-semibold'> Elegance</span>
    <br/>
@@ -18,7 +19,21 @@ Explore <span className='from-[#b1e1f3] to-[#2ba8fb] bg-clip-text text-transpare
     <Button color='primary' className=''>Get Started</Button>
     <Button color='secondary' className=''>Learn More</Button>
 </div>
-    <Divider className='mt-36'/>
+<div className='mt-32 max-w-4xl mx-auto bg-white flex text-black font-semibold py-2 px-6 rounded-md justify-between '>
+    <div className='flex gap-2 items-center'>
+<CheckCircledIcon className='w-7 h-7  ' color='lime'/>
+Open Source
+    </div>
+    <div className='flex gap-2 items-center'>
+<CheckCircledIcon className='w-7 h-7  ' color='lime'/>
+Fast Storage Engine
+    </div>
+    <div className='flex gap-2 items-center'>
+<CheckCircledIcon className='w-7 h-7  ' color='lime'/>
+Sql Based
+    </div>
+</div>
+    <Divider className='mt-24'/>
    </div>
    {/* Hello */}
    </>
